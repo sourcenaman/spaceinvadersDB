928267456b79d4245b607559879040ce494baed3
Naman Vashishth <namanvashishth@Namans-MacBook-Air.local>
2022-06-09 17:08:33 +0530

    cors error handled

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from models import Score
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ScoreData(BaseModel):
     name: constr(max_length=4)
